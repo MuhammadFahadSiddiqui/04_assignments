f2b7339ba680ad604df04c16e5096afea556813f
MuhammadFahadSiddiqui <fahadsiddiqui879@gmail.com>
2025-04-08 15:42:47 +0500

    Updated code

--- Assignments 00 to 07/00_intro_python/06_square_number/main.py
+++ Assignments 00 to 07/00_intro_python/06_square_number/main.py
@@ -1,24 +1,12 @@
-# def main():
-#     # Prompt user for a number and convert to float
-#     num = float(input("Type a number to see its square: "))
+def main():
+    # Prompt user for a number and convert to float
+    num = float(input("Type a number to see its square: "))
 
-#     print(f"{num} squared is {num ** 2}")
+    square = num * num
 
-# if __name__ == '__main__':
-#     main()
-
-from termcolor import colored
-
-def Square():
-    print(colored(" \n\t\t\t 🔢 ******* Welcome to the Square Calculator! ******* \n ", "magenta"))
-    while True:
-        try:
-            num = float(input(colored("🔢 Type a number to see its square: ", "cyan")))
-            print(colored(f"✅ {num} squared is {num ** 2}", "green"))
-            print(colored(" \n\t\t\t 🔢 ******* Code End! ******* \n ", "magenta"))
-            break
-        except ValueError:
-            print(colored("❌ Please enter a valid number", "red"))
+    print(f"{num} squared is {num ** 2}")
+    
+    print(f'{num} squared is {square} "using squre verible"')
 
 if __name__ == '__main__':
-    Square()
+    main()
